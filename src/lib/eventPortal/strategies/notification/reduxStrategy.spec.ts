9372ab44b3a5be64985ce4d09d3a5b7493746395
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2019-09-19 12:00:27 -0400

    feat: Add Redux notification strategy
    Hability to notify listeners with Redux or Redux alike APIs state
    management systems to be notified
    using it when events to whish they
    are registered arte published.

---
+++ src/lib/eventPortal/strategies/notification/reduxStrategy.spec.ts
@@ -0,0 +1,51 @@
+import test from 'ava';
+import { NotificationStrategy } from '../../../constants';
+import {
+  EventFoundation,
+  EventFoundationConstructorParams
+} from '../../../eventFoundation/EventFoundation';
+import { ReduxStrategy } from './reduxStrategy';
+
+// tslint:disable: no-let
+// setups
+let event: EventFoundation;
+let dispatchedAction: any;
+const mockStore: any = {
+  dispatch: (action: any) => (dispatchedAction = action)
+};
+
+test.before(() => {
+  const params: EventFoundationConstructorParams = {
+    eventPayload: {
+      price: 10
+    },
+    eventType: 'TEST_EVENT'
+  };
+
+  // resets
+  dispatchedAction = null;
+  event = new EventFoundation(params);
+});
+
+// tests
+test('Should create and return an ReduxStrategy object', t => {
+  t.plan(2);
+
+  const reduxStrategy: ReduxStrategy = new ReduxStrategy(mockStore);
+
+  t.is(typeof reduxStrategy, 'object');
+  t.is(reduxStrategy.constructor.name, 'ReduxStrategy');
+});
+
+test('Should create new Redux strategy', t => {
+  const reduxStrategy: ReduxStrategy = new ReduxStrategy(mockStore);
+
+  t.is(reduxStrategy.getStrategyName(), NotificationStrategy.REDUX);
+});
+
+test('Should invoke event listener notification method', t => {
+  const reduxStrategy: ReduxStrategy = new ReduxStrategy(mockStore);
+  reduxStrategy.onNotification(event.getEvent());
+
+  t.is(dispatchedAction.type, 'TEST_EVENT');
+});
---
+++ src/lib/eventPortal/strategies/notification/reduxStrategy.ts
@@ -0,0 +1,50 @@
+import { AnyAction, Store } from 'redux';
+import { NotificationStrategy } from '../../../constants';
+import { NotificationStrategyFoundation } from './notificationStrategyFoundation';
+
+/**
+ * Class representing the listeners notification solution.
+ *
+ * Abstraction supporting Redux API.
+ *
+ * Ex: Given an event publication the listener will be notified via
+ * ```js
+ *    dispatch(eventData);
+ * ```
+ */
+export class ReduxStrategy extends NotificationStrategyFoundation {
+  /**
+   * Creates the @ReduxStrategy object.
+   *
+   * @param {object} store - Redux store or Redux's store API compatible object
+   */
+  constructor(store: Store) {
+    super(store.dispatch);
+  }
+
+  /**
+   * Method invoked when published an event to which the listener is registered.
+   *
+   * @param {CustomEvent} event - Published event to send to the listener
+   */
+  public onNotification(event: CustomEvent): void {
+    this.callBack(this.createAction(event));
+  }
+
+  /**
+   * Method that return the event strategy name.
+   */
+  public getStrategyName(): string {
+    return NotificationStrategy.REDUX;
+  }
+
+  /**
+   * Method to creates Redux's actions from Custom Events.
+   *
+   * @param {CustomEvent} event - Event to be adapted into a Redux's action
+   */
+  private createAction(event: CustomEvent): AnyAction {
+    const action: AnyAction = { type: event.type, ...event.detail };
+    return action;
+  }
+}
