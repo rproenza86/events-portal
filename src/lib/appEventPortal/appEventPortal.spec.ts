459130d3994fa6043c01f1a888df0ca00c144c2e
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2019-09-23 21:47:35 -0400

    feat: Create apps event portals with logs system

---
+++ src/lib/appEventPortal/appEventPortal.spec.ts
@@ -0,0 +1,67 @@
+import test from 'ava';
+
+import {
+  EventFoundation,
+  EventFoundationConstructorParams
+} from '../eventFoundation/EventFoundation';
+import { ReduxStrategy } from '../eventPortal/strategies/notification/reduxStrategy';
+import { AppEventPortal } from './AppEventPortal';
+
+// tslint:disable: no-let
+// setups
+let event: EventFoundation;
+let dispatchedAction: any;
+let appEventPortal: AppEventPortal;
+const appName = 'SuperMicroUI';
+const mockStore: any = {
+  dispatch: (action: any) => {
+    dispatchedAction = action;
+  }
+};
+
+test.before(() => {
+  const params: EventFoundationConstructorParams = {
+    eventPayload: {
+      price: 909
+    },
+    eventType: 'TEST_APP_EVENT_PORTAL'
+  };
+
+  // resets
+  dispatchedAction = null;
+  event = new EventFoundation(params);
+
+  const reduxStrategy: ReduxStrategy = new ReduxStrategy(mockStore);
+  appEventPortal = new AppEventPortal(reduxStrategy, appName);
+  appEventPortal.listenEvent(params.eventType);
+});
+
+// tests
+test('Should publish event and notify subscriber', t => {
+  t.plan(2);
+
+  appEventPortal.notifyEvent(event.eventType, event.eventPayload);
+
+  t.is(dispatchedAction.type, 'TEST_APP_EVENT_PORTAL');
+  t.is(dispatchedAction.price, 909);
+});
+
+test('Should log the published event', t => {
+  t.plan(3);
+
+  const logs = appEventPortal.logs.getAppPublishedEvents(appName);
+
+  t.is(logs && logs.has('TEST_APP_EVENT_PORTAL'), true);
+  t.is(logs && logs.size, 1);
+  t.is(logs && logs.get('TEST_APP_EVENT_PORTAL')[0].price, 909);
+});
+
+test('Should log the notified event', t => {
+  t.plan(3);
+
+  const logs = appEventPortal.logs.getAppNotifiedEvents(appName);
+
+  t.is(logs && logs.has('TEST_APP_EVENT_PORTAL'), true);
+  t.is(logs && logs.size, 1);
+  t.is(logs && logs.get('TEST_APP_EVENT_PORTAL')[0].price, 909);
+});
---
+++ src/lib/appEventPortal/appEventPortal.ts
@@ -0,0 +1,89 @@
+import {
+  EventFoundation,
+  EventFoundationConstructorParams
+} from '../eventFoundation/EventFoundation';
+import { EventPortal } from '../eventPortal/eventPortal';
+import { NotificationStrategyFoundation } from '../eventPortal/strategies/notification/notificationStrategyFoundation';
+import { Traces } from '../traces/traces';
+
+/**
+ * Children class @EventPortal which is a Controller class to abstract the events subscription and publishing actions.
+ *
+ * This class has the login feature as an enhancement.
+ */
+export class AppEventPortal extends EventPortal {
+  /**
+   * @property {Traces} traceLogs - Traces instance used to save the publishing and notifying events by apps
+   */
+  protected readonly traceLogs: Traces = new Traces();
+  // Application name
+  private readonly appName: string;
+
+  constructor(
+    strategy: NotificationStrategyFoundation,
+    appName: string,
+    tracesInstance?: Traces
+  ) {
+    super(strategy);
+    this.appName = appName;
+    if (tracesInstance) {
+      this.traceLogs = tracesInstance;
+    }
+  }
+
+  /**
+   * Method to register/subscribe listeners to events. After this method execution the
+   * listeners/subscribers will be notified about events occurrences using the corresponding
+   * strategyCallBack.
+   *
+   * On event publishing, each the notification action will be logged into the traces instance.
+   *
+   * @param {string} eventName - The name of the event of interest
+   * @param {any} strategyCallBack - Payload object with data to send into the published event
+   */
+  public listenEvent(
+    eventName: string,
+    strategyCallBack: any = this.notificationStrategy // TODO: Check how to change the 'any' for 'NotificationStrategyFoundation' as proper type
+  ): EventPortal {
+    const callBack = (event: any) => {
+      this.traceLogs.logNotifiedEvent(this.appName, event);
+      strategyCallBack.onNotification.call(strategyCallBack, event);
+    };
+
+    this.eventTarget.subscribeEventListener(
+      eventName,
+      callBack // Avoiding scoping problems
+    );
+
+    return this;
+  }
+
+  /**
+   * Method to publish events and notify subscribers about events occurrences.
+   *
+   * Each publication action will be logged into the traces instance.
+   *
+   * @param {string} eventName - The name of the event to publish
+   * @param {object} eventPayload - Payload object with data to send into the published event
+   */
+  public notifyEvent(eventName: string, eventPayload: any): EventPortal {
+    const eventConfig: EventFoundationConstructorParams = {
+      eventPayload,
+      eventType: eventName
+    };
+    const notification = new EventFoundation(eventConfig);
+
+    this.eventTarget.publishEvent(notification.getEvent());
+    this.traceLogs.logPublishedEvent(this.appName, notification.getEvent());
+
+    return this;
+  }
+
+  /**
+   * Return an instance of the Traces class to which the
+   * app logs can be fetch.
+   */
+  public get logs(): Traces {
+    return this.traceLogs;
+  }
+}
