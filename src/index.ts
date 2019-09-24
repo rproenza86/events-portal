32dc027f3e8803becdf4624da5299ec0d9b859d9
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2019-09-24 07:01:02 -0400

    feat: Export AppsPortal on the library

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
+// Use constants to get the supported notifications strategies
 export * from './lib/constants';
+
+// Use EventPortalFactory to create an EventPortal instance
 export {
   EventPortalFactory
 } from './lib/factories/eventPortalFactory/eventPortalFactory';
+
+// Use AppsPortal to create 1...* AppEventsPortals instances with centralized traces system
+export { AppsPortal } from './lib/appsPortal/appsPortal';
